6bcf0a6d07751ea7ab4a1d3641caff38ed14dda1
T-Akshay-Raj <161284052+T-Akshay-Raj@users.noreply.github.com>
2024-04-07 22:25:45 +0530

    Add files via upload

---
+++ springboot-web-controller-basic/src/main/java/com/example/springboot/controller/basic/SpringbootControllerBasic.java
@@ -0,0 +1,11 @@
+package com.example.springboot.controller.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootControllerBasic {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootControllerBasic.class, args);
+  }
+}
---
+++ springboot-web-controller-basic/src/main/java/com/example/springboot/controller/basic/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.example.springboot.controller.basic.model;
+
+public record Greeting(long id, String content) { }
